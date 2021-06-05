241b9ef12213391f6b4ddff7422a1079b41ca87f
Weike Shi <weike.shi@ericsson.com>
2021-06-05 17:03:35 -0400

    added react routes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Route, Switch } from "react-router-dom"
 
-const App = () =>{
-    return <div>Hello World!</div>
+import Airlines from './airlines/airlines.component'
+import Airline from './airline/airline.component'
+
+const App = () => {
+    return <Switch>
+        <Route exact path="/" component={Airlines} />
+        <Route exact path="/airlines/:slug" component={Airline} />
+    </Switch>
 }
 
 export default App
---
+++ app/javascript/components/airline/airline.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airline = () => {
+    return <div>Airline#show view</div>
+}
+
+export default Airline
